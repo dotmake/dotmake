1c4f1abecc24e15151c9ce632b7335fe65d4c2bd
Nick Matantsev <nick.m@myplanetdigital.com>
2015-02-15 14:40:56 -0500

    Added bundler cache image build

--- src/createRailsInstance.js
+++ src/createRailsInstance.js
@@ -2,7 +2,11 @@
 var path = require('path');
 var spawn = require('child_process').spawn;
 var es = require('event-stream');
+var MD5 = require('MD5');
+var tar = require('tar-stream');
 var Promise = require('bluebird');
+
+var fs = require("fs");
 
 var DOCKER_ENV = {
     DOCKER_CERT_PATH: '/Users/nickmatantsev/.boot2docker/certs/boot2docker-vm',
@@ -10,7 +14,74 @@ var DOCKER_ENV = {
     DOCKER_TLS_VERIFY: '1'
 };
 
-module.exports = function createRailsInstance(srcPath) {
+function createBundleCacheImage(absPath, imageName, gemfileData, gemfileLockData) {
+    return new Promise(function (resolve, reject) {
+        console.log('building image: ' + imageName);
+
+        var buildArgs = [
+            'build',
+            '--force-rm',
+            '-t', imageName,
+            '-'
+        ];
+
+        var buildCommands = [
+            'FROM dotmake-pre-rails',
+            'ADD Gemfile /tmp/bundle-staging/',
+            'ADD Gemfile.lock /tmp/bundle-staging/',
+            'WORKDIR /tmp/bundle-staging/',
+            'RUN bundle install'
+        ];
+
+        var child = spawn('/usr/local/bin/docker', buildArgs, { env: DOCKER_ENV });
+
+        child.stdout.setEncoding('utf8');
+        child.stderr.setEncoding('utf8');
+
+        var contextTar = tar.pack();
+        contextTar.pipe(child.stdin);
+
+        contextTar.entry({ name: 'Dockerfile' }, buildCommands.join("\n"));
+        contextTar.entry({ name: 'Gemfile' }, gemfileData);
+        contextTar.entry({ name: 'Gemfile.lock' }, gemfileLockData);
+        contextTar.finalize();
+
+        es.merge(child.stdout, child.stderr).pipe(process.stdout);
+
+        child.on('close', function (code) {
+            if (code !== 0) {
+                reject(new Error('failed to build bundler cache image; docker error ' + code));
+            }
+
+            resolve(imageName);
+        });
+    });
+}
+
+function checkOrCreateBundleCacheImage(absPath) {
+    var gemfileData = fs.readFileSync(path.resolve(absPath, 'Gemfile'));
+    var gemfileLockData = fs.readFileSync(path.resolve(absPath, 'Gemfile.lock'));
+
+    var imageName = 'dotmake-bundle-cache-' + MD5(gemfileData) + '-' + MD5(gemfileLockData);
+
+    return new Promise(function (resolve, reject) {
+        console.log('checking image: ' + imageName);
+
+        var child = spawn('/usr/local/bin/docker', [], { env: DOCKER_ENV });
+
+        child.on('close', function (code) {
+            if (code === 0) {
+                // return already-built image name
+                resolve(imageName);
+            } else {
+                // trigger image creation
+                resolve(createBundleCacheImage(absPath, imageName, gemfileData, gemfileLockData));
+            }
+        });
+    });
+}
+
+function createRailsInstanceFromBundleCache(absPath, cacheImage) {
     console.log('starting Rails!');
 
     return new Promise(function (resolve, reject) {
@@ -21,10 +92,10 @@ module.exports = function createRailsInstance(srcPath) {
             '--rm',
             '--tty',
             '-p', '0.0.0.0:3000:3000',
-            '-v', path.resolve(srcPath) + ':/mount/src',
+            '-v', absPath + ':/mount/src',
             '-w', '/mount/src',
-            'rails',
-            'sh', '-c', 'bundle install && bin/rake db:migrate RAILS_ENV=development && rails server -b 0.0.0.0'
+            cacheImage,
+            'sh', '-c', 'bin/rake db:migrate RAILS_ENV=development && rails server -b 0.0.0.0'
         ];
 
         var isReady = false;
@@ -55,3 +126,12 @@ module.exports = function createRailsInstance(srcPath) {
         });
     });
 }
+
+module.exports = function createRailsInstance(srcPath) {
+    var absPath = path.resolve(srcPath);
+
+    return checkOrCreateBundleCacheImage(absPath)
+    .then(function (cacheImage) {
+        return createRailsInstanceFromBundleCache(absPath, cacheImage);
+    });
+};
